1eb644919f262bf40d340c0603a7212dcf454171
DevanshRao135 <devanshrao135@gmail.com>
2022-12-20 12:59:25 +0530

    port Commit

--- main.py
+++ main.py
@@ -171,4 +171,4 @@ def contact():
     return render_template("contact.html", parameters = parameters)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port= 5000)
